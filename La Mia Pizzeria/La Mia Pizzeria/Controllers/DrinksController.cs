7449d82b5a7c821392915ab95bd50b5bec4d7ce5
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-05-24 16:43:02 +0200

    pupush controller

---
+++ La Mia Pizzeria/La Mia Pizzeria/Controllers/DrinksController.cs
@@ -0,0 +1,13 @@
+﻿using La_Mia_Pizzeria.Database;
+using Microsoft.AspNetCore.Mvc;
+
+namespace La_Mia_Pizzeria.Controllers
+{
+    public class DrinksController : Controller
+    {
+       public IActionResult index()
+        {
+            return View();
+        }
+    }
+}
