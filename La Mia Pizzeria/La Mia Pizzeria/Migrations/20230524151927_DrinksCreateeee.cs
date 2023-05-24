7c95b7b91b9a231d792e9435fb946aeeb9583daf
stefanoreddd <stefanogg95@gmail.com>
2023-05-24 17:20:03 +0200

    Drinks Table created.

---
+++ La Mia Pizzeria/La Mia Pizzeria/Migrations/20230524151927_DrinksCreateeee.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace La_Mia_Pizzeria.Migrations
+{
+    /// <inheritdoc />
+    public partial class DrinksCreateeee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bevande",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Description = table.Column<string>(type: "text", maxLength: 400, nullable: false),
+                    ImgSource = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Price = table.Column<float>(type: "real", nullable: false),
+                    Liters = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bevande", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bevande");
+        }
+    }
+}
